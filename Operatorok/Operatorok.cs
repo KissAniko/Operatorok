5e0dd006783795ef528ab46587c2bd0e42d5b945
KissAniko <126045521+KissAniko@users.noreply.github.com>
2023-05-30 17:24:58 +0200

    folytatás

---
+++ Operatorok/Operatorok.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operatorok
+{
+    internal class Operatorok
+    {
+        //  mod, div, @, /, +, -, *, 
+
+        int bal;
+        string Operator;
+        int jobb;
+
+        public Operatorok(int bal, string @operator, int jobb)
+        {
+            this.bal = bal;
+            Operator = @operator;
+            this.jobb = jobb;
+        }
+
+        public int Bal { get => bal;  }
+        public string Operator1 { get => Operator; }
+        public int Jobb { get => jobb;  }
+    }
+}
--- Operatorok/Program.cs
+++ Operatorok/Program.cs
@@ -9,13 +9,30 @@ namespace Operatorok
 {
     internal class Program
     {
+
+        static List<string> kifejezesek;
         static void Main(string[] args)
-        {
-            var sorok = File.ReadAllLines("kifejezesek.txt");
-            foreach (var sor in sorok)
-            {
-                Console.WriteLine(sor);
-            }
+
+        { // Ellenőrzés:
+            /*     var sorok = File.ReadAllLines("kifejezesek.txt");
+                 foreach (var sor in sorok)
+                 {
+                     Console.WriteLine(sor);
+                 }
+            */
+
+
+            List<string> kifejezesek = new List<string>();
+            kifejezesek = File.ReadAllLines("kifejezesek.txt")                            
+                             .ToList();
+
+//---------------------------------------------------------------------------------------------------
+           
+            Console.WriteLine($" 2.feladat: Kifejezések száma:{kifejezesek.Count()}");
+
+//----------------------------------------------------------------------------------------------------
+
+            Console.WriteLine($"Kifejezéesek maradékos osztással: {}");
 
         }
     }
